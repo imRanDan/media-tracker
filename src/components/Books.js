6e54733852094062196415c4fb61c14b42d7ef28
Danyal Imran <dan.imran97@gmail.com>
2023-09-07 11:14:31 -0400

    working on add feature

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,28 +1,48 @@
+import { BookmarkSharp } from '@mui/icons-material';
 import React from 'react';
 import { useState } from 'react';
 
 function Books() {
-  const [books, setBooks]  = useState([]);
-  const [input, setInput] = useState('');
+  const [books, setBooks]  = useState([])
+  const [bookTitle, setBookTitle] = useState('')
+  const [bookAuthor, setBookAuthor] = useState('')
 
   return (
     <div>
       <input 
         type="text"
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        placeholder="Add a new book!"
-        />
-        <button onClick={() => {
-          setBooks([...books, input]);
-          setInput('');
-        }}>
-          Add Book
-        </button>
-   
-      {books.map((book, index) => (
-        <p key={index}>{book}</p>
+        value={bookTitle}
+        onChange={e => setBookTitle(e.target.value)}
+        placeholder="Book Title"
+      />
+      <input
+        type="text"
+        value={bookAuthor}
+        onChange={e => setBookAuthor(e.target.value)}
+        placeholder="Author's name"
+      />
+      <button onClick={() => {
+        const newBook = {
+          id: Date.now(),
+          title: bookTitle,
+          author: bookAuthor,
+          status: "unread"
+        }
+        setBooks([...books, newBook])
+        setBookTitle('')
+        setBookAuthor('')
+      }}>
+        Add Book
+      </button>
+
+      {books.map((book) => (
+        <div key = {book.id}>
+          <p>Title: {book.title}</p>
+          <p>Author: {book.author}</p>
+          <p>Status: {book.status}</p>
+        </div>
       ))}
+
     </div>
   )
 }

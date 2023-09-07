13cd3a96bbaf55af029283e19b3ccb2cdc1bb544
Danyal Imran <dan.imran97@gmail.com>
2023-09-07 11:03:16 -0400

    added in a feature to add books, but not saved anywhere

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Books() {
+  const [books, setBooks]  = useState([]);
+  const [input, setInput] = useState('');
+
   return (
     <div>
-      <p>*insert books here*</p>
+      <input 
+        type="text"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        placeholder="Add a new book!"
+        />
+        <button onClick={() => {
+          setBooks([...books, input]);
+          setInput('');
+        }}>
+          Add Book
+        </button>
+   
+      {books.map((book, index) => (
+        <p key={index}>{book}</p>
+      ))}
     </div>
   )
 }
 
 export default Books
+
+
+

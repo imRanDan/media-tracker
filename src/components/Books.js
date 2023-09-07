1a1ea8ed37818526d18a3dece85f5a55e4eb269f
Danyal Imran <dan.imran97@gmail.com>
2023-09-07 11:31:09 -0400

    adding in an edit feature for Status

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,26 @@
-import { BookmarkSharp } from '@mui/icons-material';
 import React from 'react';
 import { useState } from 'react';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Books() {
   const [books, setBooks]  = useState([])
   const [bookTitle, setBookTitle] = useState('')
   const [bookAuthor, setBookAuthor] = useState('')
+  const [bookStatus, setBookStatus] = useState('Reading')
+  // Adding Books
+
+
+  // Editing Entries
+  const [editingId, setEditingId] = useState(null)
+  const updateStatus = (id, newStatus) => {
+    setBooks((prevBooks) =>
+      prevBooks.map((book) =>
+        book.id === id ? {...book, status: newStatus } : book
+      )
+    )
+  }
 
   return (
     <div>
@@ -21,16 +36,23 @@ function Books() {
         onChange={e => setBookAuthor(e.target.value)}
         placeholder="Author's name"
       />
+      <select value={bookStatus} onChange={(e) => setBookStatus(e.target.value)}>
+        <option value="Reading">Reading</option>
+        <option value="Completed">Completed</option>
+        <option value="Wishlist">Wishlist</option>
+        <option value="Abandoned">Abandoned</option>
+      </select>
       <button onClick={() => {
         const newBook = {
           id: Date.now(),
           title: bookTitle,
           author: bookAuthor,
-          status: "unread"
+          status: bookStatus
         }
         setBooks([...books, newBook])
         setBookTitle('')
         setBookAuthor('')
+        setBookStatus('Reading')
       }}>
         Add Book
       </button>
@@ -40,9 +62,25 @@ function Books() {
           <p>Title: {book.title}</p>
           <p>Author: {book.author}</p>
           <p>Status: {book.status}</p>
+          <button onClick={() => setEditingId(book.id)}>Edit</button>
+          {editingId === book.id ? (
+          <>
+            <select 
+              value={book.status}
+              onChange={(e) => updateStatus(book.id, e.target.value)}
+            >
+              <option value="Reading">Reading</option>
+              <option value="Completed">Completed</option>
+              <option value="Wishlist">Wishlist</option>
+              <option value="Abandoned">Abandoned</option>
+            </select>
+            <button onClick={() => setEditingId(null)}>Done</button>
+            </>
+          ) : (
+            <p>Status: {book.status}</p>
+          )}
         </div>
       ))}
-
     </div>
   )
 }
